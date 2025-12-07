3523b644623dbe8d256c070f8f84768e9f8fe9c8
TonyLcfOclock <tony.lecerf@oclock.school>
2025-12-07 18:35:22 +0100

    Création du fichier server.js et séparation de la partie client et la partie serveur

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log('Serveur démarré.');
+});
